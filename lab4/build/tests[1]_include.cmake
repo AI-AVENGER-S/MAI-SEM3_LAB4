2acd5d649af040f9e2d6ad16f722facb9b8d9525
artems <whard8y@gmail.com>
2025-11-07 22:32:57 +0300

    Delete build

--- lab4/build/tests[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/Users/artems/Desktop/c++_labs/testing/lab4/build/tests[1]_tests.cmake")
-  include("/Users/artems/Desktop/c++_labs/testing/lab4/build/tests[1]_tests.cmake")
-else()
-  add_test(tests_NOT_BUILT tests_NOT_BUILT)
-endif()
